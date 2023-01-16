1a69e719c3edb0714257bee0d127f8349b3135f6
Ralph Etienne <ralphyetienne@yahoo.com>
2023-01-16 14:09:04 -0500

    added redux to app and created navslice within the redux store

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+
+
+// Set up Redux
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  <Provider store={store}>
+     <View style={styles.container}>
+      <Text>Hello Everyone!</Text>
     </View>
+  </Provider>
   );
 }
 
