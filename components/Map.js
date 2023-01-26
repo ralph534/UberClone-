501ad3abb596e43eee140180806795995440abdf
Ralph Etienne <ralphyetienne@yahoo.com>
2023-01-26 09:45:11 -0500

    crested a select a ride function which allows the user to choose either X, XL, or LUX ride and calculate the pricing from time and distance of travel

--- components/Map.js
+++ components/Map.js
@@ -2,8 +2,8 @@ import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useRef } from 'react'
 import MapView, { Marker } from 'react-native-maps'
 import tw from 'tailwind-react-native-classnames'
-import { selectDestination, selectOrigin } from '../slices/navSlice'
-import { useSelector } from 'react-redux'
+import { selectDestination, selectOrigin, setTravelTimeInfo } from '../slices/navSlice'
+import { useDispatch, useSelector } from 'react-redux'
 import MapViewDirections from 'react-native-maps-directions'
 import { GOOGLE_MAPS_APIKEY } from "@env";
 
@@ -14,6 +14,8 @@ const Map = () => {
 
     const mapRef = useRef(null)
 
+    const dispatch = useDispatch()
+
     useEffect(() => {
      if (!origin || !destination) return;
 
@@ -22,6 +24,22 @@ const Map = () => {
         edgePadding: {top: 50, right: 50, bottom: 50, left: 50}
      })
     },[origin, destination])
+
+
+    useEffect(() => {
+     if(!origin || !destination) return;
+
+        const getTravelTime = async () => {
+          fetch(
+           `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`)
+           .then(res => res.json())
+         .then((data) => {
+            dispatch(setTravelTimeInfo(data.rows[0].elements[0]))
+         })
+        }
+
+        getTravelTime()
+    },[origin, destination, GOOGLE_MAPS_APIKEY])
 
 
   return (
--- components/RideOptionCard.js
+++ components/RideOptionCard.js
@@ -1,23 +1,102 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import React, { useState } from 'react'
 import tw from 'tailwind-react-native-classnames'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
 import { Icon } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
 import NavigatorCard from '../components/NavigatorCard'
+import { useSelector } from 'react-redux'
+import { selectTravelTimeInfo } from '../slices/navSlice'
 
 
 const RideOptionCard = () => {
 
     const navigate = useNavigation()
+
+    const [selected, setSelected] = useState(null)
+
+    const travelTimeInformation = useSelector(selectTravelTimeInfo)
+
+    const data = [
+        {
+            id:'Uber-X-123',
+            title: 'UberX',
+            multipler: '1',
+            image: 'https://links.papareact.com/3pn'
+        },
+        {
+            id: 'Uber-XL-456',
+            title: 'Uber-XL',
+            multipler: '1.2',
+            image: 'https://links.papareact.com/5w8'
+        },
+        {
+            id: 'Uber-LUX-789',
+            title: 'UberLUX',
+            multipler: '1.75',
+            image: 'https://links.papareact.com/7pf'
+        }
+    ]
+
+   const SURCHARGE_RATE = 1.5;
+
+
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
         <View>
             <TouchableOpacity style={tw` absolute top-3 left-5 z-50 p-3 rounded-full`} >
                 <Icon name='chevron-left' type='fontawesome' />
             </TouchableOpacity>
-          <Text style={tw`text-center py-5 text-lg `}>Select A Ride</Text>
+          <Text style={tw`text-center py-5 text-lg `}>Select A Ride - {travelTimeInformation?.distance?.text}</Text>
         </View>
+
+        <FlatList 
+        data={data} 
+        keyExtractor={(item) => item.id}
+        renderItem={({item: { id, title, multipler, image}, item}) => (
+           <TouchableOpacity 
+           onPress={() => setSelected(item)}
+           style={tw`flex-row items-center justify-between px-10
+            ${id === selected?.id && 'bg-gray-200'}`}>
+            <Image 
+            style={{
+                width:100,
+                height: 100,
+                resizeMode: 'contain'
+            }}
+            source={{ uri: image}}
+            />
+
+          <View style={tw`-ml-6`}>
+            <Text style={tw`text-xl font-semibold`}>{ title }</Text>
+            <Text>{travelTimeInformation?.duration?.text} Travel Time</Text>
+          </View>
+
+            <Text style={tw`text-xl`}>
+
+             {new Intl.NumberFormat('en', {
+                style:'currency',
+                currency:'USD',
+
+             }).format(
+                (travelTimeInformation?.duration?.value * SURCHARGE_RATE * multipler) / 100
+
+             )}
+
+            </Text>
+
+         </TouchableOpacity> 
+        )}
+         />
+
+         <View>
+            <TouchableOpacity disabled={!selected} style={tw`bg-black py-3 m-3 ${!selected && 'bg-gray-300'}`}>
+              <Text style={tw`text-center text-white text-xl`}>
+                Choose{selected?.title}
+              </Text>
+            </TouchableOpacity>
+         </View>
+
     </SafeAreaView>
   )
 }
